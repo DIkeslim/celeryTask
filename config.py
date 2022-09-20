b6ba073892222901d9ed84bfb67da737a34f6c0a
Dikeslim <chimdike.ironkwe@gmail.com>
2022-09-20 09:35:41 +0100

    changes to config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-SECRET_KEY = 'Chimdike123'
+SECRET_KEY = 'some-secret-key'
 
 MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = 'chimdike'
-MAIL_PASSWORD = 'Chimdike1'
+MAIL_USERNAME = 'mailUsername'
+MAIL_PASSWORD = 'mailPassword'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
